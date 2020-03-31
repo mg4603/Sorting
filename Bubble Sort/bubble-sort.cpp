d9881fa77001c13f30d0b71a5d9d2a0a83fbaf41
Michael George <mg4603@bennett.edu.in>
2020-03-31 10:23:26 +0530

    optimized code for bubble sort

--- Bubble Sort/bubble-sort.cpp
+++ Bubble Sort/bubble-sort.cpp
@@ -16,12 +16,17 @@ void printArr(int arr[], int n);
 
 void bubbleSort(int arr[], int n){
 
-	
+	bool flag = false;
 	for(int i = 0; i < n - 1; i++){
+		
 		for(int j = 0; j < n - i - 1; j++){
 			if(arr[j] > arr[j+1]){
 				swap(arr[j],arr[j+1]);
+				flag = true;
 			}
+		}
+		if(!flag){
+			break;
 		}
 	}
 
